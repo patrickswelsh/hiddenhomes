191367e6ad0a06c908bb407c62172000c46d7a57
Patrick Welsh <patrickswelsh@gmail.com>
2024-01-21 21:03:21 +0000

    misc updates - login settings

---
+++ pages/User Managment/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,12 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+async signInWithEmail() {
+  const { data, error } = await supabase.
+  })
+}
+}
